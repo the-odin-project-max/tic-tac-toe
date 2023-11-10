7e8bc6f249ace10267c0b6991f95dad86244f95b
Max <max.teiger@gmail.com>
2023-11-10 18:43:02 +0100

    feat(ia): code a simple bot to play with

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+let difficulty = "easy";
+
+document.querySelectorAll(".difficulty-button").forEach((button) => {
+	button.addEventListener("click", (e) => {
+		difficulty = e.target.id;
+		gameLogic.initGame();
+	});
+});
+
+
+
 const gameBoard = (() => {
 	let board = ["", "", "", "", "", "", "", "", ""];
 
@@ -15,22 +26,22 @@ const gameBoard = (() => {
 		return { getName, getMark };
 	};
 
-	function checkWin() {
-		if (board[0] === board[1] && board[1] === board[2] && board[0] !== "") {
+	function checkWin(testBoard = board) {
+		if (testBoard[0] === testBoard[1] && testBoard[1] === testBoard[2] && testBoard[0] !== "") {
 			return true;
-		} else if (board[3] === board[4] && board[4] === board[5] && board[3] !== "") {
+		} else if (testBoard[3] === testBoard[4] && testBoard[4] === testBoard[5] && testBoard[3] !== "") {
 			return true;
-		} else if (board[6] === board[7] && board[7] === board[8] && board[6] !== "") {
+		} else if (testBoard[6] === testBoard[7] && testBoard[7] === testBoard[8] && testBoard[6] !== "") {
 			return true;
-		} else if (board[0] === board[3] && board[3] === board[6] && board[0] !== "") {
+		} else if (testBoard[0] === testBoard[3] && testBoard[3] === testBoard[6] && testBoard[0] !== "") {
 			return true;
-		} else if (board[1] === board[4] && board[4] === board[7] && board[1] !== "") {
+		} else if (testBoard[1] === testBoard[4] && testBoard[4] === testBoard[7] && testBoard[1] !== "") {
 			return true;
-		} else if (board[2] === board[5] && board[5] === board[8] && board[2] !== "") {
+		} else if (testBoard[2] === testBoard[5] && testBoard[5] === testBoard[8] && testBoard[2] !== "") {
 			return true;
-		} else if (board[0] === board[4] && board[4] === board[8] && board[0] !== "") {
+		} else if (testBoard[0] === testBoard[4] && testBoard[4] === testBoard[8] && testBoard[0] !== "") {
 			return true;
-		} else if (board[2] === board[4] && board[4] === board[6] && board[2] !== "") {
+		} else if (testBoard[2] === testBoard[4] && testBoard[4] === testBoard[6] && testBoard[2] !== "") {
 			return true;
 		} else {
 			return false;
@@ -40,36 +51,119 @@ const gameBoard = (() => {
 	return { getBoard, setBoard, resetBoard, createPlayer, checkWin };
 })();
 
-const displayController = (() => {
+const IAPlayer = (() => {
+	const playEasy = (freeCells) => {
+		const randomIndex = Math.floor(Math.random() * freeCells.length);
+		return randomIndex;
+	};
+
+	const playMedium = (board) => {
+		// Check for a winning move
+		for (let i = 0; i < board.length; i++) {
+			if (board[i] === "") {
+				// Simulate making the move and check for a win
+				board[i] = "O"; // Assuming the computer is playing as "O"
+				if (gameBoard.checkWin(board)) {
+					board[i] = ""; // Reset the board
+					return i; // Return the winning move
+				}
+				board[i] = ""; // Reset the board
+			}
+		}
+
+		// Check for a blocking move (preventing the player from winning)
+		for (let i = 0; i < board.length; i++) {
+			if (board[i] === "") {
+				// Simulate making the move and check for a block
+				board[i] = "X"; // Assuming the player is "X"
+				if (gameBoard.checkWin(board)) {
+					board[i] = ""; // Reset the board
+					console.log("block, playing ", i);
+					return i; // Return the blocking move
+				}
+				board[i] = ""; // Reset the board
+			}
+		}
+
+		// Play first free cell if no winning or blocking moves are available
+		let index = board.indexOf("");
+		return index;
+		// const randomIndex = Math.floor(Math.random() * freeCells.length);
+		// return randomIndex;
+	};
+	const playHard = (board) => {
+		const randomIndex = Math.floor(Math.random() * freeCells.length);
+		freeCells[randomIndex].click();
+	};
+	const playImpossible = (board) => {
+		const randomIndex = Math.floor(Math.random() * freeCells.length);
+		freeCells[randomIndex].click();
+	};
+
+	return { playEasy, playMedium, playHard, playImpossible };
+})();
+
+const gameLogic = (() => {
 	const player1 = gameBoard.createPlayer("Player 1", "X");
 	const player2 = gameBoard.createPlayer("Player 2", "O");
 	let currentPlayer = player1;
 
 	let freeCells = document.querySelectorAll(".cell:not(.occupied)");
 
-	const playerClickOnCell = (e) => {
-		const cell = e.target;
-
+	const cellClicked = (cell) => {
 		cell.removeEventListener("click", playerClickOnCell);
 		cell.textContent = currentPlayer.getMark();
 		cell.classList.add("occupied");
 
 		gameBoard.setBoard(cell.dataset.index, currentPlayer.getMark());
+	};
+
+
+	const playerClickOnCell = (e) => {
+		const cell = e.target;
+		cellClicked(cell);
 
 		// Update freeCells
 		freeCells = document.querySelectorAll(".cell:not(.occupied)");
 
+		// Check for win or tie
 		if (gameBoard.checkWin()) {
-			alert(`${currentPlayer.getName()} wins!`);
-			initGame();
+			setTimeout(() => {
+				alert(`${currentPlayer.getName()} wins!`);
+				initGame();
+			}, 500);
 		} else if (freeCells.length === 0) {
-			alert("It's a tie!");
-			initGame();
+			setTimeout(() => {
+				alert("It's a tie!");
+				initGame();
+			}, 500);
 		} else {
 			currentPlayer = currentPlayer === player1 ? player2 : player1;
-			freeCells.forEach(cell => {
-				cell.addEventListener("click", playerClickOnCell);
-			});
+			document.querySelector("#status").innerHTML = `Current player: ${currentPlayer.getName()}, difficulty: ${difficulty}`;
+		}
+
+		// IA Player
+		if (currentPlayer === player2) {
+			setTimeout(() => {
+				if (difficulty == "easy") {
+					freeCells[IAPlayer.playEasy(freeCells)].click();
+				} else if (difficulty == "medium") {
+					console.log("playing medium");
+					let cells = document.querySelectorAll(".cell");
+					let indexToPlay = IAPlayer.playMedium(gameBoard.getBoard());
+					cells[indexToPlay].click();
+				} else if (difficulty == "hard") {
+					console.log("playing hard");
+					let cells = document.querySelectorAll(".cell");
+					let indexToPlay = IAPlayer.playMedium(gameBoard.getBoard());
+					cells[indexToPlay].click();
+				} else if (difficulty == "impossible") {
+					console.log("playing impossible");
+					let cells = document.querySelectorAll(".cell");
+					let indexToPlay = IAPlayer.playImpossible(gameBoard.getBoard());
+					cells[indexToPlay].click();
+				}
+			}, 500);
 		}
 
 	}
@@ -83,10 +177,11 @@ const displayController = (() => {
 		});
 		gameBoard.resetBoard();
 		currentPlayer = player1;
+		document.querySelector("#status").innerHTML = `Current player: ${currentPlayer.getName()}, difficulty: ${difficulty}`;
 	}
 
 	return { initGame };
 })();
 
 
-displayController.initGame();
+gameLogic.initGame();
